97800585252eb4e7522c5aaa74ee93475574572e
Andrew Wladis <andrewwladis@gmail.com>
2020-03-31 17:12:32 -0400

    updated

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,28 @@
 const DuckyComics = "Ducky-Comics"
 const assets = [
-  "/",
-  "/index.html"
+    "/",
+    "/index.html",
+    "/index.css",
+    "/manifest.json",
+    "/comics.html",
+    "/start.html",
+    "/readorder.html",
+    "/reviews.html",
+    "/season-one.html",
+    "/TeenTitansSeasonOne.html",
+    "/season-two.html",
+    "/safari-pinned-tab.svg",
+    "/mstile-150x150.png",
+    "/favicon.ico",
+    "/favicon-32x32.png",
+    "/serviceWorker.js"
 ]
 
 self.addEventListener("install", installEvent => {
+    console.log("Trying to install servivce worker and in cache static assets");
   installEvent.waitUntil(
     caches.open(DuckyComics).then(cache => {
-      cache.addAll(assets)
+      return cache.addAll(assets)
     })
   )
 })
